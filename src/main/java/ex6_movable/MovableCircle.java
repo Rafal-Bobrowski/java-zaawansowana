50738c47790739505f8c12e10e996b7313745e52
Rafal Bobrowski <rafal.bobrowski95@gmail.com>
2022-04-22 12:52:13 +0200

    Solved 6 exercise. Add Movable.java interface, MovablePoint.java and MovableCircle.java. Both additionally extends Point2D and Circle

---
+++ src/main/java/ex6_movable/MovableCircle.java
@@ -0,0 +1,51 @@
+package ex6_movable;
+
+import ex3_shape.Circle;
+
+public class MovableCircle extends Circle implements Movable {
+    private MovablePoint centerPoint;
+    private float radius;
+
+    public MovableCircle(float radius, MovablePoint centerPoint) {
+        super(radius);
+        this.centerPoint = centerPoint;
+    }
+
+    public MovableCircle(float radius, float x, float y, float xSpeed, float ySpeed) {
+        super(radius);
+        this.centerPoint = new MovablePoint(x, y, xSpeed, ySpeed);
+    }
+
+    public MovableCircle(String color, boolean isFilled, float radius, MovablePoint centerPoint) {
+        super(color, isFilled, radius);
+        this.centerPoint = centerPoint;
+    }
+
+    public MovablePoint getCenterPoint() {
+        return centerPoint;
+    }
+
+    public void setCenterPoint(MovablePoint centerPoint) {
+        this.centerPoint = centerPoint;
+    }
+
+    @Override
+    public void moveUp() {
+        centerPoint.moveUp();
+    }
+
+    @Override
+    public void moveDown() {
+        centerPoint.moveDown();
+    }
+
+    @Override
+    public void moveLeft() {
+        centerPoint.moveLeft();
+    }
+
+    @Override
+    public void moveRight() {
+        centerPoint.moveRight();
+    }
+}
---
+++ src/main/java/ex6_movable/MovablePoint.java
@@ -0,0 +1,65 @@
+package ex6_movable;
+
+import ex1_point.Point2D;
+
+public class MovablePoint extends Point2D implements Movable {
+
+    protected float xSpeed, ySpeed;
+
+    public MovablePoint(float xSpeed, float ySpeed) {
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public MovablePoint(float x, float y, float xSpeed, float ySpeed) {
+        super(x, y);
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public float getxSpeed() {
+        return xSpeed;
+    }
+
+    public void setxSpeed(float xSpeed) {
+        this.xSpeed = xSpeed;
+    }
+
+    public float getySpeed() {
+        return ySpeed;
+    }
+
+    public void setySpeed(float ySpeed) {
+        this.ySpeed = ySpeed;
+    }
+
+    @Override
+    public void moveUp() {
+        y += ySpeed;
+    }
+
+    @Override
+    public void moveDown() {
+        y -= ySpeed;
+    }
+
+    @Override
+    public void moveLeft() {
+        x -= xSpeed;
+    }
+
+    @Override
+    public void moveRight() {
+        x += xSpeed;
+    }
+
+    @Override
+    public String toString() {
+        return "MovablePoint{" +
+                "x=" + x +
+                ", y=" + y +
+                ", xSpeed=" + xSpeed +
+                ", ySpeed=" + ySpeed +
+                '}';
+    }
+}
